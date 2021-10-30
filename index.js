6d689c84339a0e569fd7f31b50ae4059726aa203
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-10-30 13:54:10 +0600

    Place Order Api Created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,6 +20,7 @@ async function run() {
         await client.connect();
         const database = client.db('travel-guide');
         const destinationCollections = database.collection('tour-destination');
+        const orderCollections = database.collection('orders');
 
 
         //GET Tour Destination using API
@@ -28,6 +31,15 @@ async function run() {
             res.send(destinations);
         })
 
+        //Get API to load a single tour destination using ID
+        app.get('/tourDestinations/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const destination = await destinationCollections.findOne(query);
+
+            res.send(destination);
+        })
+
         //Post API to post new Tour Destinations
         app.post('/addTourDestination', async(req,res) => {
             const destination = req.body;
@@ -35,6 +47,15 @@ async function run() {
             const result = await destinationCollections.insertOne(destination);
             res.json(result);
 
+        })
+
+        //Add Order Api
+        app.post('/orders', async(req,res) => {
+            const order = req.body;
+
+            const result = await orderCollections.insertOne(order);
+
+            res.json(result);
         })
     }
     finally{
